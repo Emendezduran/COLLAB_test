a4889fa2074103c744b635ac8044fb485f3529ea
emendezduran <emendezduran@nautilus25.danielcastelao.org>
2018-10-16 09:06:57 +0200

    Final b2

---
+++ collab_test/COLLAB_test.java
@@ -0,0 +1,20 @@
+package collab_test;
+
+import java.util.Scanner;
+
+public class COLLAB_test {
+
+    public static void main(String[] args) {
+   
+   
+    System.out.println("escribe tu nombre:");
+    Scanner leernombre = new Scanner (System.in);
+    String nombre = leernombre.next();
+    
+    System.out.println("Hola" +nombre+"!");
+     
+        
+    }
+    
+}
+        
